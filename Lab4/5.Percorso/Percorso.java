6a2539c695c0742881452121100b6e273a8d3f36
Valentina Botti <56971455+ValeBotti@users.noreply.github.com>
2024-04-16 14:45:33 +0200

    prova

---
+++ Lab4/5.Percorso/Percorso.java
@@ -0,0 +1,27 @@
+public class Percorso {
+//OVERVIEW: modella una percorso costituito da una sequenza di dati di tipo Punto
+
+//attributes
+
+//constructors
+
+//methods
+    public void add(){
+    //MODIFIES: this (p)
+    //EFFECTS: adds a point at the end of the path
+    }
+
+    public void remove(){
+    //MODIFIES: this (p)
+    //EFFECTS: removes the last point at the end of the path
+    }
+
+    public Double length(){
+    //EFFECTS: calculates the length of the path
+    }
+
+    
+    public static void main(String args[]) {
+
+    }
+}
